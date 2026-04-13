a765be721c53ec1c2ac18278e7fd30257eaf5ae6
zhangtao <git.vistaflyer.com>
2026-04-13 19:11:48 +0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,9 +17,11 @@ export default function Footer() {
             <p className="text-sm leading-relaxed text-slate-400 max-w-xs">
               {t('description')}
             </p>
+
             <a href="https://theresanaiforthat.com/ai/ytvidhub/?ref=featured&v=7340698" target="_blank" rel="nofollow"><Image width={300} height={54} src="https://media.theresanaiforthat.com/featured-on-taaft.png?width=600" alt="Featured on TAAFT" /></a>
-            <a href="https://openhunts.com" target="_blank" title="OpenHunts Club" > <Image alt="OpenHunts Club Member" height={105} src="https://cdn.openhunts.com/badges/club.webp"  width={486} /></a>
- 
+           <a href="https://openhunts.com" target="_blank" title="OpenHunts Club">
+  <Image alt="OpenHunts Club Member" height={105} src="https://cdn.openhunts.com/badges/club.webp" width={486} />
+</a>
 
           </div>
 
